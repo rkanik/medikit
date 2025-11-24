d20cf5dc8ce330f8efdb67a53e6233c112091977
Rezwana Tasnin <79830581+encoderit-anik@users.noreply.github.com>
2025-11-24 14:40:24 +0600

    Added gluestack

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,36 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
 
-import { useColorScheme } from '@/hooks/use-color-scheme';
+import { Box } from '@/components/ui/box';
+import { Button, ButtonText } from '@/components/ui/button';
+import { GluestackUIProvider } from '@/components/ui/gluestack-ui-provider';
+import '@/global.css';
+import { useState } from 'react';
 
 export const unstable_settings = {
   anchor: '(tabs)',
 };
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-
+  const [colorMode, setColorMode] = useState<'light' | 'dark'>('light');
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <GluestackUIProvider mode={colorMode}>
+      <StatusBar style={colorMode} />
+      <Box className="bg-primary-500 p-16">
+        <Button
+          onPress={() => {
+            setColorMode(colorMode === 'light' ? 'dark' : 'light');
+          }}
+        >
+          <ButtonText>Toggle color mode</ButtonText>
+        </Button>
+      </Box>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
       </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    </GluestackUIProvider>
+
   );
 }
