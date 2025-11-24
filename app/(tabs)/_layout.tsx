a75547ed6d9b73ae63055fcc5a5a570c347e4d48
Rezwana Tasnin <79830581+encoderit-anik@users.noreply.github.com>
2025-11-24 23:18:12 +0600

    Added Icons

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,56 +1,64 @@
-import { Tabs } from "expo-router";
-import React from "react";
+import { Tabs } from 'expo-router'
+import React from 'react'
 
-import { IconSymbol } from "@/components/ui/icon-symbol";
-import { useColorSchemeStorage } from "@/hooks/useColorSchemeStorage";
+import { Icon } from '@/components/ui/icon'
+import { useColorSchemeStorage } from '@/hooks/useColorSchemeStorage'
+import {
+	CloudUploadIcon,
+	HomeIcon,
+	ListTodoIcon,
+	MenuIcon,
+} from 'lucide-react-native'
 
 export default function TabLayout() {
-  const { colorScheme } = useColorSchemeStorage();
-  return (
-    <Tabs
-      screenOptions={{
-        headerShown: false,
-        tabBarStyle: {
-          backgroundColor: colorScheme === "dark" ? "#000" : "#fff",
-        },
-      }}
-    >
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: "Home",
-          tabBarIcon: ({ color }) => (
-            <IconSymbol size={28} name="house.fill" color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="menu"
-        options={{
-          title: "Menu",
-          tabBarIcon: ({ color }) => (
-            <IconSymbol size={28} name="list.bullet" color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="records"
-        options={{
-          title: "Records",
-          tabBarIcon: ({ color }) => (
-            <IconSymbol size={28} name="doc.text.fill" color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="backup"
-        options={{
-          title: "Backup",
-          tabBarIcon: ({ color }) => (
-            <IconSymbol size={28} name="icloud.and.arrow.up.fill" color={color} />
-          ),
-        }}
-      />
-    </Tabs>
-  );
+	const { colorScheme } = useColorSchemeStorage()
+	return (
+		<Tabs
+			screenOptions={{
+				headerShown: false,
+				tabBarStyle: {
+					height: 96,
+					paddingTop: 16,
+					backgroundColor: colorScheme === 'dark' ? '#000' : '#fff',
+				},
+			}}
+		>
+			<Tabs.Screen
+				name="index"
+				options={{
+					title: 'Home',
+					tabBarIcon: ({ color }) => (
+						<Icon as={HomeIcon} size="xl" color={color} />
+					),
+				}}
+			/>
+			<Tabs.Screen
+				name="menu"
+				options={{
+					title: 'Menu',
+					tabBarIcon: ({ color }) => (
+						<Icon as={MenuIcon} size="xl" color={color} />
+					),
+				}}
+			/>
+			<Tabs.Screen
+				name="records"
+				options={{
+					title: 'Records',
+					tabBarIcon: ({ color }) => (
+						<Icon as={ListTodoIcon} size="xl" color={color} />
+					),
+				}}
+			/>
+			<Tabs.Screen
+				name="backup"
+				options={{
+					title: 'Backup',
+					tabBarIcon: ({ color }) => (
+						<Icon as={CloudUploadIcon} size="xl" color={color} />
+					),
+				}}
+			/>
+		</Tabs>
+	)
 }
