2620b316ae1df71ef6cb049ad66251831e49f6df
Rezwana Tasnin <79830581+encoderit-anik@users.noreply.github.com>
2025-12-15 19:02:28 +0600

    Fix theme provider

--- components/ThemeProvider/index.tsx
+++ components/ThemeProvider/index.tsx
@@ -1,26 +1,14 @@
 import { useColorSchemeStorage } from '@/hooks/useColorSchemeStorage'
+import { useColorScheme } from 'nativewind'
 import { PropsWithChildren, useEffect } from 'react'
-import { Appearance, ColorSchemeName } from 'react-native'
 
 export type TThemeProviderProps = PropsWithChildren
 
 export const ThemeProvider = ({ children }: TThemeProviderProps) => {
 	const { colorScheme } = useColorSchemeStorage()
+	const { setColorScheme } = useColorScheme()
 	useEffect(() => {
-		if (colorScheme === 'system') {
-			const listener = (preferences?: Appearance.AppearancePreferences) => {
-				Appearance.setColorScheme(
-					preferences?.colorScheme || Appearance.getColorScheme(),
-				)
-			}
-			listener()
-			const subscription = Appearance.addChangeListener(listener)
-			return () => {
-				subscription.remove()
-			}
-		} else {
-			Appearance.setColorScheme(colorScheme as ColorSchemeName)
-		}
-	}, [colorScheme])
+		setColorScheme(colorScheme)
+	}, [colorScheme, setColorScheme])
 	return children
 }
