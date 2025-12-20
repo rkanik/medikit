dbf8d3bd76b2843ead8cd94aaaca63b04f352939
Rezwana Tasnin <79830581+encoderit-anik@users.noreply.github.com>
2025-12-21 01:12:36 +0600

    Enhance Input component by adding clear button functionality and improving structure with TouchableOpacity and View wrappers.

--- components/ui/input/index.tsx
+++ components/ui/input/index.tsx
@@ -2,11 +2,13 @@ import type { TextInputProps } from 'react-native'
 import type { VariantProps } from 'tailwind-variants'
 
 import { forwardRef, useState } from 'react'
-import { TextInput } from 'react-native'
+import { TextInput, TouchableOpacity, View } from 'react-native'
 
 import { tv } from 'tailwind-variants'
 
 import { useSchemeColors } from '@/hooks/useSchemeColors'
+
+import { Icon } from '../icon'
 
 export const inputVariants = tv({
 	base: 'rounded-lg flex-row items-center justify-between overflow-hidden',
@@ -57,11 +59,13 @@ export type TInputProps = TextInputProps &
 export const Input = forwardRef<TextInput, TInputProps>(function Input(
 	{
 		error,
+		value,
 		className,
 		size = 'base',
 		variant = 'base',
 		onBlur,
 		onFocus,
+		onChangeText,
 		...props
 	},
 	ref,
@@ -69,25 +73,40 @@ export const Input = forwardRef<TextInput, TInputProps>(function Input(
 	const [focus, setFocus] = useState(false)
 	const { textColorSecondary } = useSchemeColors()
 	return (
-		<TextInput
-			{...props}
-			ref={ref}
-			className={inputVariants({
-				size,
-				error,
-				focus,
-				variant,
-				className,
-			})}
-			placeholderTextColor={textColorSecondary}
-			onFocus={e => {
-				onFocus?.(e)
-				setFocus(true)
-			}}
-			onBlur={e => {
-				onBlur?.(e)
-				setFocus(false)
-			}}
-		/>
+		<View className="relative">
+			<TextInput
+				{...props}
+				ref={ref}
+				value={value}
+				onChangeText={onChangeText}
+				placeholderTextColor={textColorSecondary}
+				className={inputVariants({
+					size,
+					error,
+					focus,
+					variant,
+					className,
+				})}
+				onFocus={e => {
+					onFocus?.(e)
+					setFocus(true)
+				}}
+				onBlur={e => {
+					onBlur?.(e)
+					setFocus(false)
+				}}
+			/>
+			{value && (
+				<TouchableOpacity
+					className="absolute right-px top-px bottom-px px-3 bg-white dark:bg-neutral-800 rounded-r-lg items-center justify-center"
+					onPress={() => onChangeText?.('')}
+				>
+					<Icon
+						name="x"
+						className="text-lg text-neutral-500 dark:text-neutral-400"
+					/>
+				</TouchableOpacity>
+			)}
+		</View>
 	)
 })
